de8f463d4c9e1f9a7414622b1121cae0eedb1520
unknown <B00140092@mytudublin.ie>
2023-10-11 11:11:18 +0100

    Finished part3

--- Part3.php
+++ Part3.php
@@ -1,1 +1,37 @@
-{\rtf1}
+<?php
+//Give the name of the program here
+//Include your name and the date here
+//Give a brief description of what the program does
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tennisclub";
+$port = 3306;
+
+//set the default timezone - this is necessary since MySQL 8. This is an effort to store all dates and times together with their timezones. 
+//This is particularly important where there is a timestamp indicating when something happened.
+date_default_timezone_set('Europe/Dublin');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, firstName, surName FROM member";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstName"]. " " . $row["surName"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_query($conn, $sql);
+
+mysqli_close($conn);
+?>
